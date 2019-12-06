facdb9975607e9936a48db87a96bfb3d6a42b85b
Sam Kortekaas <skorteka@f1r6s12.codam.nl>
2019-12-06 12:33:33 +0100

    Uncommented code for dangling pointers in main.c

--- main.c
+++ main.c
@@ -7,15 +7,59 @@ int main(void)
 {
 	char *line;
 	int out = 1;
-	int fd = open("text.txt", O_RDONLY | O_EXCL);
-
-	line = (char *)0x37624236487;
-	while (out == 1)
+	int fd;
+	int i = 0;
+	while (i < 100)
 	{
-		out = get_next_line(fd, &line);
-		printf("[OUT %i][%s]\n", out, line);
-		free(line);
+		out = 1;
+		fd = open("../get_next_line/get_next_line.c", O_RDONLY | O_EXCL);
+
+		while (out == 1)
+		{
+			out = get_next_line(fd, &line);
+			printf("[OUT %i][%s]\n", out, line);
+			free(line);
+		}
+		close(fd);
+		i++;
 	}
-	close(fd);
+
+	// char *line;
+	// int out = 1;
+	// int fd = open("../get_next_line/get_next_line.c", O_RDONLY | O_EXCL);
+
+	// while (out == 1)
+	// {
+	// 	out = get_next_line(fd, &line);
+	// 	printf("[OUT %i][%s]\n", out, line);
+	// 	free(line);
+	// }
+	// close(fd);
+
+	// char *line1;
+	// out = 1;
+	// fd = open("../get_next_line/get_next_line.c", O_RDONLY | O_EXCL);
+
+	// while (out == 1)
+	// {
+	// 	out = get_next_line(fd, &line1);
+	// 	printf("[OUT %i][%s]\n", out, line1);
+	// 	free(line1);
+	// }
+	// close(fd);
+
+	// char *line2;
+	// out = 1;
+	// fd = open("../get_next_line/get_next_line.c", O_RDONLY | O_EXCL);
+
+	// while (out == 1)
+	// {
+	// 	out = get_next_line(fd, &line2);
+	// 	printf("[OUT %i][%s]\n", out, line2);
+	// 	free(line2);
+	// }
+	// close(fd);
+
+
 	return (0);
 }
